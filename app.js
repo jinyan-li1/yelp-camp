5e404b4108b59fbae88a4fb5cb323abb4c8a05d7
jli <jinyan_li@yahoo.com>
2023-04-30 18:22:22 -0700

    updated mongo atlas url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const { application } = require("express");
 
 const MongoStore = require('connect-mongo')(session);
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp" // process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 
 mongoose.connect(dbUrl, {
